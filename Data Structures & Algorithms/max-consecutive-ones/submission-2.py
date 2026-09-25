482f090b3f4688ed4c5538fbcb67f6fcd174ab84
Grant Oxford <112038019+gnart-gnart@users.noreply.github.com>
2026-09-25 13:32:40 -0500

    Add: max-consecutive-ones - submission-2

---
+++ Data Structures & Algorithms/max-consecutive-ones/submission-2.py
@@ -0,0 +1,10 @@
+class Solution:
+    def findMaxConsecutiveOnes(self, nums: List[int]) -> int:
+        result = count = 0
+        for n in nums:
+            if n == 1:
+                count += 1
+            else:
+                count = 0
+            result = max(count, result)
+        return result
